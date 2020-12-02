59b1210c303659e88ec345edb256c47fd84e8453
Jan Diehm <jadiehm@gmail.com>
2020-12-02 13:05:41 -0600

    meta and fonts

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,10 +1,10 @@
 <svelte:head>
-  <title>Svelte Starter</title>
+  <title>Neighborhood Cat Bingo</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="" />
-  <meta name="news_keywords" content="" />
+  <meta name="description" content="A cat lovers game to make your neighborhood strolls more competitive." />
+  <meta name="news_keywords" content="cat, cats, walk, bingo, game, neighborhood, svelte" />
 
   <meta property="og:title" content="" />
   <meta property="og:site_name" content="" />
